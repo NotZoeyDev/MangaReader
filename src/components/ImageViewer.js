cf322fdf857ee0c0d7439b60d53d36c112ce2390
Zoey Désautels <zoey@panties.moe>
2018-07-15 14:51:28 -0400

    We got an icon now

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -38,51 +38,50 @@ module.exports = class ImageViewer {
     }
 
     loadFolder(folderpath) {
-        fs.readdir(folderpath, (err, _files) => {
-            this.folder = folderpath;
-            let files = [];
+        let _files = fs.readdirSync(folderpath);
+        this.folder = folderpath;
+        let files = [];
 
-            // Filter out non-image files
-            for(let file of _files) {
-                let filePath = `${this.folder}/${file}`;
-                let fileStat = fs.statSync(filePath);
+        // Filter out non-image files
+        for (let file of _files) {
+            let filePath = `${this.folder}/${file}`;
+            let fileStat = fs.statSync(filePath);
 
-                // Check if the current "file" is a file
-                if(fileStat.isFile()) {
-                    let imagesExtension = [
-                        ".png", ".jpg", ".gif"
-                    ];
-                    let fileExtension = path.extname(filePath);
+            // Check if the current "file" is a file
+            if (fileStat.isFile()) {
+                let imagesExtension = [
+                    ".png", ".jpg", ".gif"
+                ];
+                let fileExtension = path.extname(filePath);
 
-                    // Check that the file is an image
-                    if(imagesExtension.includes(fileExtension)) files.push(filePath);
-                }
+                // Check that the file is an image
+                if (imagesExtension.includes(fileExtension)) files.push(filePath);
             }
-            
-            // Sort files via their timestamp
-            files.sort((a, b) => {
-                return fs.statSync(a).mtime.getTime() - fs.statSync(b).mtime.getTime();
-            });
+        }
 
-            // Check if we have any files to load
-            if(files.length == 0) {
-                dialog.showMessageBox(Window.AppWindow, {
-                    type: "error",
-                    title: "Error",
-                    message: "No images were found in the selected folder."
-                }, () => {
-                    if(this.files.length == 0) this.openFolderDialog();
-                })
-            } else {
-                this.index = 0;
-                this.files = files;
-                if(ImagesBox.shown) {
-                    ImagesBox.unloadImages();
-                    ImagesBox.loadImages(() => {});
-                }
-                this.loadPage();
-            } 
+        // Sort files via their timestamp
+        files.sort((a, b) => {
+            return fs.statSync(a).mtime.getTime() - fs.statSync(b).mtime.getTime();
         });
+
+        // Check if we have any files to load
+        if (files.length == 0) {
+            dialog.showMessageBox(Window.AppWindow, {
+                type: "error",
+                title: "Error",
+                message: "No images were found in the selected folder."
+            }, () => {
+                if (this.files.length == 0) this.openFolderDialog();
+            })
+        } else {
+            this.index = 0;
+            this.files = files;
+            if (ImagesBox.shown) {
+                ImagesBox.unloadImages();
+                ImagesBox.loadImages(() => { });
+            }
+            this.loadPage();
+        }
     }
 
     // Load a folder
@@ -90,10 +89,10 @@ module.exports = class ImageViewer {
         dialog.showOpenDialog(Window.AppWindow, {
             properties: ["openDirectory"]
         }, (filePaths, bookmarks) => {
-            if(filePaths && filePaths.length > 0) {
+            if (filePaths && filePaths.length > 0) {
                 this.loadFolder(filePaths[0]);
             } else {
-                if(this.folder == "") remote.app.exit(0);
+                if (this.folder == "") remote.app.exit(0);
             }
         });
     }
